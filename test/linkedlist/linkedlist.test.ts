b802d8d1c6fd39298c15b9a7e0197bf581628117
Vikrant Singh <vikrantbhadauriya2@gmail.com>
2024-11-18 13:11:31 +0530

    [Update] test for method definition

--- test/linkedlist/linkedlist.test.ts
+++ test/linkedlist/linkedlist.test.ts
@@ -5,21 +5,90 @@ describe("LinkedList", () => {
 	let list: LinkedList<number>;
 
 	beforeEach(() => {
-		list = new LinkedList(new LinkedListNode(23));
+		list = new LinkedList();
 	});
 
-	it('should have addLast method', () => {
-		expect(list.addLast).toBeDefined();
-		expect(typeof list.addLast).toBe('function');
+	describe('should have method named: ', () => {
+		it('add', () => {
+			expect(list.add).toBeDefined();
+			expect(typeof list.add).toBe('function');
+		});
+
+		it('addFirst', () => {
+			expect(list.addFirst).toBeDefined();
+			expect(typeof list.addFirst).toBe('function');
+		});
+
+		it('addLast', () => {
+			expect(list.addLast).toBeDefined();
+			expect(typeof list.addLast).toBe('function');
+		});
+
+		it('print', () => {
+			expect(list.print).toBeDefined();
+			expect(typeof list.print).toBe('function');
+		});
+
+		it('len', () => {
+			expect(list.len).toBeDefined();
+			expect(typeof list.len).toBe('function');
+		});
+
+		it('clear', () => {
+			expect(list.clear).toBeDefined();
+			expect(typeof list.clear).toBe('function');
+		});
+
+		it('get', () => {
+			expect(list.get).toBeDefined();
+			expect(typeof list.get).toBe('function');
+		});
+
+		it('getFirst', () => {
+			expect(list.getFirst).toBeDefined();
+			expect(typeof list.getFirst).toBe('function');
+		});
+
+		it('getLast', () => {
+			expect(list.getLast).toBeDefined();
+			expect(typeof list.getLast).toBe('function');
+		});
+
+		it('indexOf', () => {
+			expect(list.indexOf).toBeDefined();
+			expect(typeof list.indexOf).toBe('function');
+		});
+
+		it('lastIndexOf', () => {
+			expect(list.lastIndexOf).toBeDefined();
+			expect(typeof list.lastIndexOf).toBe('function');
+		});
+
+		it('remove', () => {
+			expect(list.remove).toBeDefined();
+			expect(typeof list.remove).toBe('function');
+		});
+
+		it('removeFirst', () => {
+			expect(list.removeFirst).toBeDefined();
+			expect(typeof list.removeFirst).toBe('function');
+		});
+
+		it('removeLast', () => {
+			expect(list.removeLast).toBeDefined();
+			expect(typeof list.removeLast).toBe('function');
+		});
+
+		it('toArray', () => {
+			expect(list.toArray).toBeDefined();
+			expect(typeof list.toArray).toBe('function');
+		});
 	});
 
-	it('should have length method', () => {
-		expect(list.length).toBeDefined();
-		expect(typeof list.length).toBe('function');
-	});
 
 	it('should add element to the last', () => {
+		list.addLast(new LinkedListNode(10));
 		list.addLast(new LinkedListNode(20));
-		expect(list.length()).toBe(2)
+		expect(list.len()).toBe(2)
 	});
 });
